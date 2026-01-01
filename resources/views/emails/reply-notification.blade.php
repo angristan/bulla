a4face26a8690b583076fe7e951f99e0acdad22f
Stanislas Lange <git@slange.me>
2026-01-01 19:44:32 +0100

    fix: improve email template styling
    - Remove Laravel copyright footer
    - Use Bulla logo in header
    - Use panel components for quoted comments
    - Remove duplicate horizontal rule above unsubscribe

--- resources/views/emails/reply-notification.blade.php
+++ resources/views/emails/reply-notification.blade.php
@@ -4,16 +4,20 @@
 Someone replied to your comment on {{ $siteName }}.
 
 **{{ $reply->author ?? 'Anonymous' }}** wrote:
-> {{ Str::limit(strip_tags($reply->body_html), 300) }}
+
+<x-mail::panel>
+{{ Str::limit(strip_tags($reply->body_html), 300) }}
+</x-mail::panel>
 
 In reply to your comment:
-> {{ Str::limit(strip_tags($parentComment->body_html), 150) }}
+
+<x-mail::panel>
+{{ Str::limit(strip_tags($parentComment->body_html), 150) }}
+</x-mail::panel>
 
 <x-mail::button :url="$threadUrl">
 View Reply
 </x-mail::button>
-
----
 
 <x-mail::subcopy>
 [Unsubscribe from this comment]({{ $unsubscribeUrl }}) | [Unsubscribe from all]({{ $unsubscribeAllUrl }})
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ rtrim(config('app.url'), '/') }}/bulla.png" class="logo" alt="{{ config('app.name') }}" style="max-width: 150px; height: auto;">
+<img src="{{ $url }}/bulla.png" class="logo" alt="Bulla Logo">
 </a>
 </td>
 </tr>
